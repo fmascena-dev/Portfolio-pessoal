740fead3ece719a25d61f1c306b00fdc02fa038b
felipe mascena <felipe.mascena.dev@gmail.com>
2024-08-09 16:30:05 -0300

    portfolio

---
+++ js/script.js
@@ -0,0 +1,11 @@
+
+
+const menuHamburguer = document.querySelector('.menu-hamburguer');
+menuHamburguer.addEventListener('click', () => {
+    toggleMenu();
+});
+
+function toggleMenu() {
+    const nav = document.querySelector('.nav-responsive');
+    console.log(nav);
+}
